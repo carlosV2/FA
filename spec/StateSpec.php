5a723f81f9ac9e6c72119e2cbd8fd2ad7b1e8278
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2016-04-02 12:13:56 +0100

    Allow states to know their outputs

--- spec/StateSpec.php
+++ spec/StateSpec.php
@@ -2,6 +2,9 @@
 
 namespace spec\carlosV2\NFA;
 
+use carlosV2\NFA\State;
+use carlosV2\NFA\Symbol;
+use carlosV2\NFA\Transition;
 use PhpSpec\ObjectBehavior;
 
 class StateSpec extends ObjectBehavior
@@ -9,5 +12,24 @@ class StateSpec extends ObjectBehavior
     function it_is_not_a_final_state()
     {
         $this->shouldNotBeFinal();
+    }
+
+    function it_cretes_a_transition_on_an_output_symbol(Symbol $symbol)
+    {
+        $this->on($symbol)->shouldBeAnInstanceOf(Transition::class);
+    }
+
+    function it_returns_the_states_reachable_by_a_symbol(Symbol $symbol1, Symbol $symbol2, State $state1, State $state2, State $state3)
+    {
+        $symbol1->matches($symbol1)->willReturn(true);
+        $symbol1->matches($symbol2)->willReturn(false);
+        $symbol2->matches($symbol1)->willReturn(false);
+        $symbol2->matches($symbol2)->willReturn(true);
+
+        $this->on($symbol1)->visit($state1);
+        $this->on($symbol2)->visit($state2);
+        $this->on($symbol1)->visit($state3);
+
+        $this->getReachableStatesBySymbol($symbol1)->shouldReturn([$state1, $state3]);
     }
 }
--- src/State.php
+++ src/State.php
@@ -5,10 +5,50 @@ namespace carlosV2\NFA;
 class State
 {
     /**
+     * @var Transition[]
+     */
+    private $transitions;
+
+    public function __construct()
+    {
+        $this->transitions = [];
+    }
+
+    /**
      * @return bool
      */
     public function isFinal()
     {
         return false;
+    }
+
+    /**
+     * @param Symbol $symbol
+     *
+     * @return Transition
+     */
+    public function on(Symbol $symbol)
+    {
+        $transition = new Transition($symbol);
+        $this->transitions[] = $transition;
+
+        return $transition;
+    }
+
+    /**
+     * @param Symbol $symbol
+     *
+     * @return State[]
+     */
+    public function getReachableStatesBySymbol(Symbol $symbol)
+    {
+        $states = [];
+        foreach ($this->transitions as $transition) {
+            if ($transition->isGranted($symbol)) {
+                $states[] = $transition->getState();
+            }
+        }
+
+        return $states;
     }
 }
