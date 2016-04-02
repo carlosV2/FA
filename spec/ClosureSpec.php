2eb71ce3cc3e801b3998a9ae7801a9f0cd5fd4a2
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2016-04-02 12:37:15 +0100

    Add a closure object

---
+++ spec/ClosureSpec.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace spec\carlosV2\NFA;
+
+use carlosV2\NFA\Closure;
+use carlosV2\NFA\EpsilonSymbol;
+use carlosV2\NFA\State;
+use carlosV2\NFA\Symbol;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ClosureSpec extends ObjectBehavior
+{
+    function let(State $state1, State $state2, State $state3, State $state4)
+    {
+        $state1->getReachableStatesBySymbol(Argument::type(EpsilonSymbol::class))->willReturn([$state3]);
+        $state2->getReachableStatesBySymbol(Argument::type(EpsilonSymbol::class))->willReturn([$state4]);
+        $state3->getReachableStatesBySymbol(Argument::type(EpsilonSymbol::class))->willReturn([$state4]);
+        $state4->getReachableStatesBySymbol(Argument::type(EpsilonSymbol::class))->willReturn([]);
+
+        $this->beConstructedThrough('forStates', [[$state1->getWrappedObject(), $state2->getWrappedObject()]]);
+    }
+
+    function it_is_final_if_at_least_one_state_is_final(State $state1, State $state2, State $state3, State $state4)
+    {
+        $state1->isFinal()->willReturn(false);
+        $state2->isFinal()->willReturn(false);
+        $state3->isFinal()->willReturn(true);
+        $state4->isFinal()->willReturn(false);
+
+        $this->shouldBeFinal();
+    }
+
+    function it_is_not_final_if_there_are_not_final_states(State $state1, State $state2, State $state3, State $state4)
+    {
+        $state1->isFinal()->willReturn(false);
+        $state2->isFinal()->willReturn(false);
+        $state3->isFinal()->willReturn(false);
+        $state4->isFinal()->willReturn(false);
+
+        $this->shouldNotBeFinal();
+    }
+
+    function it_returns_the_advanced_closure_for_a_symbol(Symbol $symbol, State $state1, State $state2, State $state3, State $state4)
+    {
+        $state1->getReachableStatesBySymbol($symbol)->willReturn([$state2]);
+        $state2->getReachableStatesBySymbol($symbol)->willReturn([$state3]);
+        $state3->getReachableStatesBySymbol($symbol)->willReturn([$state4]);
+        $state4->getReachableStatesBySymbol($symbol)->willReturn([$state1]);
+
+        $this->advance($symbol)->shouldBeAnInstanceOf(Closure::class);
+    }
+}
---
+++ src/Closure.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace carlosV2\NFA;
+
+class Closure
+{
+    /**
+     * @var State[]
+     */
+    private $states;
+
+    /**
+     * @param State[] $states
+     */
+    private function __construct(array $states)
+    {
+        $this->states = $states;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFinal()
+    {
+        foreach ($this->states as $state) {
+            if ($state->isFinal()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param Symbol $symbol
+     *
+     * @return Closure
+     */
+    public function advance(Symbol $symbol)
+    {
+        $closure = new self([]);
+        foreach ($this->states as $state) {
+            $closure->addStates($state->getReachableStatesBySymbol($symbol));
+        }
+
+        $closure->addReachableStatesByEpsilonSymbol();
+        return $closure;
+    }
+
+    private function addReachableStatesByEpsilonSymbol()
+    {
+        for ($i = 0; $i < count($this->states); $i++) {
+            $this->addStates($this->states[$i]->getReachableStatesBySymbol(EpsilonSymbol::create()));
+        }
+    }
+
+    /**
+     * @param State[] $states
+     */
+    private function addStates(array $states)
+    {
+        foreach ($states as $state) {
+            $this->addState($state);
+        }
+    }
+
+    /**
+     * @param State $state
+     */
+    private function addState(State $state)
+    {
+        foreach ($this->states as $internal) {
+            if ($internal === $state) {
+                return;
+            }
+        }
+
+        $this->states[] = $state;
+    }
+    /**
+     * @param State[] $states
+     *
+     * @return Closure
+     */
+    public static function forStates(array $states)
+    {
+        $closure = new self($states);
+        $closure->addReachableStatesByEpsilonSymbol();
+
+        return $closure;
+    }
+}
