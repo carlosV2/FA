152838879db70b30613fe53352ccf6e9543f8f2b
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2016-04-03 23:12:58 +0100

    Add a transition for DFA automaton

---
+++ spec/DFA/TransitionSpec.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace spec\carlosV2\FA\DFA;
+
+use carlosV2\FA\DFA\State;
+use carlosV2\FA\Symbol;
+use PhpSpec\ObjectBehavior;
+
+class TransitionSpec extends ObjectBehavior
+{
+    function let(Symbol $symbol)
+    {
+        $this->beConstructedWith($symbol);
+    }
+
+    function it_grants_permission_to_a_symbol_if_the_symbol_matches_it(Symbol $symbol, Symbol $other)
+    {
+        $symbol->matches($other)->willReturn(true);
+
+        $this->shouldBeGranted($other);
+    }
+
+    function it_does_not_grant_permission_to_a_symbol_if_the_symbol_does_not_match_it(Symbol $symbol, Symbol $other)
+    {
+        $symbol->matches($other)->willReturn(false);
+
+        $this->shouldNotBeGranted($other);
+    }
+
+    function it_exposes_state(State $state)
+    {
+        $this->visit($state);
+        $this->getState()->shouldReturn($state);
+    }
+}
---
+++ src/DFA/Transition.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace carlosV2\FA\DFA;
+
+use carlosV2\FA\Symbol;
+
+class Transition
+{
+    /**
+     * @var Symbol
+     */
+    private $symbol;
+
+    /**
+     * @var State
+     */
+    private $state;
+
+    /**
+     * @param Symbol $symbol
+     */
+    public function __construct(Symbol $symbol)
+    {
+        $this->symbol = $symbol;
+    }
+
+    /**
+     * @param Symbol $symbol
+     *
+     * @return bool
+     */
+    public function isGranted(Symbol $symbol)
+    {
+        return $this->symbol->matches($symbol);
+    }
+
+    /**
+     * @param State $state
+     */
+    public function visit(State $state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return State
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+}
